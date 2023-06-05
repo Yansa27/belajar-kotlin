560efb486b915bdb2970318abdf601afafd62c36
yansa27 <juliansa837@gmail.com>
2023-06-05 18:57:29 +0700

    day 2

--- src/tipeDataArray.kt
+++ src/tipeDataArray.kt
@@ -1,1 +1,23 @@
 package src
+
+fun main() {
+    val members: Array<String> = arrayOf("Eko" , "Joko" , "Budi")
+    val values: Array<Byte> = arrayOf(100 , 90 , 95)
+    val balances: Array<Int> = arrayOf(10_000 , 20_000, 30_000)
+
+    println(members)
+    println(values)
+    println(balances)
+
+    /* Operasi Array
+    * size > Untuk mendapatkan panjang Array
+    * get(index) > Mendapat data di posisi index
+    * [index] > mendapat data di posisi index
+    *  set(index , value) mengubah data di posisi index
+    * [index] = value > mengubah data di posisi index
+    */
+
+    println(members.size)
+    println(values.get(0))
+    println(balances[1])
+}
